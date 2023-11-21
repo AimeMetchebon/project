95f5e5ea563b274f1c3222ab9209f92d2f65244b
AimeMetchebon <144697898+AimeMetchebon@users.noreply.github.com>
2023-11-21 22:32:00 +0100

    Add files via upload

---
+++ car_streamlit.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 19:33:51 2023
+
+@author: metch
+"""
+import streamlit as st
+import pandas as pd
+
+import matplotlib.pyplot as plt
+# une analyse de corrélation et de distribution grâce à différents graphiques et des commentaires.
+
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_cars = pd.read_csv(link)
+st.write(df_cars)
+
+st.write('Anlyse de correlation')
+
+
+import seaborn as sns
+
+
+pays = st.radio(
+    "Choisissez le pays pour voir le graphique de correlation",
+    ["US", "Europe", "Japan"],
+    captions = ["Etats Unis Ameriques", "Europe", "Japan"])
+
+if pays == 'US':
+    viz_correlation = sns.heatmap(df_cars[df_cars.continent.str.contains('US.')].corr(), 
+    								center=0,
+    								cmap = sns.color_palette("vlag", as_cmap=True)
+    								)
+
+    st.pyplot(viz_correlation.figure)
+
+    plt.close()
+
+    viz_correlation = sns.pairplot(df_cars[df_cars.continent.str.contains('US.')]						
+    								)
+    								
+    st.pyplot(viz_correlation.figure)
+    
+    plt.close()
+    
+elif pays == 'Europe':
+    viz_correlation = sns.heatmap(df_cars[df_cars.continent.str.contains('Europe.')].corr(), 
+    								center=0,
+    								cmap = sns.color_palette("vlag", as_cmap=True)
+    								)
+
+    st.pyplot(viz_correlation.figure)
+
+    plt.close()
+
+    viz_correlation = sns.pairplot(df_cars[df_cars.continent.str.contains('Europe.')]						
+    								)
+    								
+    st.pyplot(viz_correlation.figure)
+    plt.close()
+    
+else:
+    viz_correlation = sns.heatmap(df_cars[df_cars.continent.str.contains('Japan.')].corr(), 
+    								center=0,
+    								cmap = sns.color_palette("vlag", as_cmap=True)
+    								)
+
+    st.pyplot(viz_correlation.figure)
+
+    plt.close()
+
+    viz_correlation = sns.pairplot(df_cars[df_cars.continent.str.contains('Japan.')]						
+    								)
+    								
+    st.pyplot(viz_correlation.figure)
+ 
+       
+    plt.close()
+
+
+    st.write('corelation negative entre ,mpg et cylinders, mpg et hp, mpg et cubicinches, mpg et cylinders')
+
+    st.write('correlation positive entre cubicinches et culindres, cylinders et weightlbs')
+
+#viz_correlation = sns.heatmap(df_cars[df_cars['continent']=='Japan.'].corr(), 
+#								center=0,
+#								cmap = sns.color_palette("vlag", as_cmap=True)
+#								)
+#st.pyplot(viz_correlation.figure)
+
+#plt.close()
+
+
+#viz_correlation = sns.pairplot(df_cars, hue ='continent'						
+#								)
+#								
+#st.pyplot(viz_correlation.figure)
+
+#st.write(df_cars.loc["Europe.",:])
